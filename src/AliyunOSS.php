b046d98cf56f38fc61f376fc6e572d39ba0fb9d3
Seven <lovevipdsw@outlook.com>
2016-09-02 19:57:11 +0800

    添加文件名称。

---
+++ src/AliyunOSS.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Medz\Component\AliyunOSS;
+
+use Medz\Component\StreamWrapper\AliyunOSS\AliyunOSS as AliyunOssClient;
+
+/**
+ * Aliyun OSS StreamWrapper alias encapsulation.
+ *
+ * 因为原包名称主要针对StreamWrapper的封装
+ * 容易让人产生误解，曲解了包的作用。
+ * 所以针对原有包做一个别名包。
+ * 
+ * @author Seven Du <lovevipdsw@outlook.com>
+ **/
+class AliyunOSS extends AliyunOssClient
+{
+} // END class AliyunOSS extends AliyunOssClient
