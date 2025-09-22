a879c6d789b547c1e46800d92786a95f2f546a42
Seven Du <5564821+medz@users.noreply.github.com>
2025-09-22 14:40:18 +0800

    Export resetMemoizedFor from memoized module

--- lib/oref.dart
+++ lib/oref.dart
@@ -1,7 +1,7 @@
 export 'src/core/computed.dart' show computed;
 export 'src/core/effect.dart' show effect;
 export 'src/core/effect_scope.dart' show effectScope;
-export 'src/core/memoized.dart' show useMemoized;
+export 'src/core/memoized.dart' show useMemoized, resetMemoizedFor;
 export 'src/core/signal.dart' show signal;
 export 'src/core/widget_effect.dart' show useWidgetEffect, WidgetEffect;
 export 'src/core/widget_scope.dart' show useWidgetScope, WidgetScope;
