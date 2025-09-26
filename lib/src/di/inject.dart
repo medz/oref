e7f213fae3ad1feeafcc30385d5a6fb28a2b71e1
Seven Du <5564821+medz@users.noreply.github.com>
2025-09-26 09:17:55 +0800

    WIP

--- lib/src/di/inject.dart
+++ lib/src/di/inject.dart
@@ -1,1 +1,3 @@
+import 'package:flutter/widgets.dart';
 
+abstract interface class Dependency<T> implements Element {}
--- lib/src/di/provide.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-void provide<T>(BuildContext? context, Symbol name, T value) {
-  WeakReference;
-}
