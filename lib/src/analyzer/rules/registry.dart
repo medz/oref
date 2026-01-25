c6678d8ab93f02d551a461f7724b01b9d14c4884
Seven Du <5564821+medz@users.noreply.github.com>
2026-01-26 02:41:57 +0800

    Add analyzer plugin to enforce context in widget builds

--- lib/src/analyzer/rules/registry.dart
+++ lib/src/analyzer/rules/registry.dart
@@ -1,3 +1,7 @@
 import 'package:analysis_server_plugin/registry.dart';
 
-void registerRules(PluginRegistry registry) {}
+import 'signal_requires_context_rule.dart';
+
+void registerRules(PluginRegistry registry) {
+  registry.registerWarningRule(SignalRequiresContextRule());
+}
